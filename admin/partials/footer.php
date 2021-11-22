2449ea3b71f37c56a0537f96e4ffd8ccffbbf6ce
unknown <xz2715@nyu.edu>
2021-11-21 21:54:01 -0500

    create partials for header and footer

---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+        <!-- Footer Section Starts -->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Some Restaurant. Developed By - @EddieSource</p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+    </body>
+</html>
