472a93dee35ab8ffa43ceab5eecf63157863d2fb
unknown <xz2715@nyu.edu>
2021-11-22 21:48:17 -0500

    ingest dummy data for admin table

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -5,6 +5,46 @@
             <div class="wrapper">
                 <h1>Manage Admin</h1>
 
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1.</td>
+                        <td>Xianhui Zhu</td>
+                        <td>EddieSource</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2.</td>
+                        <td>Xianhui Zhu</td>
+                        <td>EddieSource</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3.</td>
+                        <td>Xianhui Zhu</td>
+                        <td>EddieSource</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+
+                </table>
+
             </div>
         </div>
         <!-- Main Content Section Ends -->
